399212cf444c6ca339003c38a8f3679f3c1eeab0
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-03 19:48:50 +0300

    version sort

--- web.rb
+++ web.rb
@@ -10,6 +10,16 @@ require 'erb'
 require 'sinatra/cache'
 require 'redis-sinatra'
 
+module Enumerable
+  def version_sort
+    sort_by { |key,val|
+       key.gsub(/_SP/,'.').gsub(/_Factory/,'_100').split(/_/) \
+          .map { |v| v =~ /\A\d+(\.\d+)?\z/ ? -(v.to_f) : v.downcase }
+    }
+  end
+end
+
+
 class SingingRain < Sinatra::Base
   register(Sinatra::Cache)
 
