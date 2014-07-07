e01ec883a36133184adb5493753bc066a45ee793
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-07 22:17:52 +0300

    up

--- web.rb
+++ web.rb
@@ -4,8 +4,7 @@ require "bundler"
 Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
 
 require_relative './badge_downloader'
-require 'sinatra/contrib/all'
-require 'rack'
+require 'sinatra/streaming'
 require 'securerandom'
 
 class RubygemsDownloadShieldsApp < Sinatra::Base
@@ -31,13 +30,8 @@ class RubygemsDownloadShieldsApp < Sinatra::Base
        send_file File.join(settings.public_folder, "favicon.ico"), :disposition => 'inline', :type => "image/x-icon"
      else
       stream :keep_open do |out|           
-        EM.run do
           @downloader = BadgeDownloader.new( params, out)
           @downloader.download_shield
-        end
-        EM.error_handler{ |e|
-          puts   " Error raised during event loop: #{e.message}"
-        }
       end
       
     end
