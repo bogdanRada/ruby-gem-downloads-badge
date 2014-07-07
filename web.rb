603ab14f5c084e92b6f3ee0488b53c86d7923513
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-07 20:33:29 +0300

    up

--- web.rb
+++ web.rb
@@ -22,7 +22,7 @@ class RubygemsDownloadShieldsApp < Sinatra::Base
   
   before do
     content_type "image/svg+xml;  Content-Encoding: gzip; charset=utf-8; "
-    cache_control :no_cache
+    cache_control :no_cache, :must_revalidate, :max_age => 0
     etag SecureRandom.hex
   end
 
