e5996f359ed6c22ce749d5c59016158c6d36475b
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-08 09:05:35 +0300

    up'

--- badge_downloader.rb
+++ badge_downloader.rb
@@ -37,8 +37,6 @@ class BadgeDownloader
     resp = @badge_conn.get do |req|
       req.url "/badge/downloads-#{count}-#{@color}.svg#{@style}"
       req.headers['Content-Type'] = "image/svg+xml; Content-Encoding: gzip; charset=utf-8;"
-      req.headers['Cache-Control'] = "no-cache"
-      req.headers['Pragma'] = "no-cache"
       req.options.timeout = 5           # open/read timeout in seconds
       req.options.open_timeout = 2
     end
--- web.rb
+++ web.rb
@@ -5,15 +5,15 @@ Bundler.require :default, (ENV["RACK_ENV"] || "development").to_sym
 
 require_relative './badge_downloader'
 require 'sinatra/streaming'
+require "sinatra/json"
+require 'json'
 require 'securerandom'
 require 'erb'
 
 class RubygemsDownloadShieldsApp < Sinatra::Base
-  register(Sinatra::Cache)
   helpers Sinatra::Streaming
    
   
-  set :cache_enabled, false 
   set :static_cache_control, [:no_cache, :must_revalidate, :max_age => 0]
   set :static, false                            # set up static file routing
   set :public_folder, File.expand_path('../static', __FILE__)# set up the static dir (with images/js/css inside)
@@ -29,13 +29,13 @@ class RubygemsDownloadShieldsApp < Sinatra::Base
 
   get '/?:gem?/?:version?'  do
     if !params[:gem].nil? &&  params[:gem].include?("favicon")
-       send_file File.join(settings.public_folder, "favicon.ico"), :disposition => 'inline', :type => "image/x-icon"
-     else
+      send_file File.join(settings.public_folder, "favicon.ico"), :disposition => 'inline', :type => "image/x-icon"
+    else
       stream :keep_open do |out|  
-         EM.run {         
+        EM.run { 
           @downloader = BadgeDownloader.new( params, out)
           @downloader.download_shield
-        }
+          }
         EM.error_handler{|e| puts "Error during event loop : #{e.inspect}" }
       end
     end
