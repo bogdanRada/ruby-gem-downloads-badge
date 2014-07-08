3045f98c19ad0909bfc079edecfb19feeecd785a
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-08 18:06:33 +0300

    up

--- web.rb
+++ web.rb
@@ -29,7 +29,7 @@ class RubygemsDownloadShieldsApp < Sinatra::Base
     content_type "image/svg+xml;  Content-Encoding: gzip; charset=utf-8; "
     cache_control :no_cache, :must_revalidate, :max_age => 0
     etag SecureRandom.hex
-    last_modified(Time.now)
+    last_modified(Time.now - 60)
   end
 
   get '/?:gem?/?:version?'  do
