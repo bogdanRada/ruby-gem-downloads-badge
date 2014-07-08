cada4ceab7fcdedbb97589528ec0d4f792ae36d8
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-08 18:15:00 +0300

    expires

--- web.rb
+++ web.rb
@@ -28,9 +28,10 @@ class RubygemsDownloadShieldsApp < Sinatra::Base
   before do
     content_type "image/svg+xml;  Content-Encoding: gzip; charset=utf-8; "
     cache_control :no_cache, :must_revalidate, :max_age => 0
-    etag SecureRandom.hex
-    last_modified(Time.now - 60)
-    expires 0
+    headers({"Pragma" => "no-cache"})
+#    etag SecureRandom.hex
+#    last_modified(Time.now - 60)
+    expires Time.now - 1
   end
 
   get '/?:gem?/?:version?'  do
