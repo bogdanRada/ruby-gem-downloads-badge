328b80d1be16b1321f78e682468fccdd83667ffa
Rada Bogdan <raoul_ice@yahoo.com>
2014-07-08 11:15:54 +0300

    up

--- config/initializers/faraday_no_cache_middleware.rb
+++ config/initializers/faraday_no_cache_middleware.rb
@@ -9,7 +9,6 @@ class FaradayNoCacheMiddleware
     @app.call(env).on_complete do
         env[:response_headers]["Cache-Control"] =  "no-cache, no-store, max-age=0, must-revalidate"
         env[:response_headers]["Pragma"] = "no-cache"
-        env[:response_headers]["Expires"] = Time.now 
   end
        
   end
