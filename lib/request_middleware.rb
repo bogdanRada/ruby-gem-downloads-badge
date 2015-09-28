63a0e1571c92b0e7930e37e91dac7f3c236fc743
Rada Bogdan <raoul_ice@yahoo.com>
2015-09-28 11:10:15 +0300

    update

--- lib/request_middleware.rb
+++ lib/request_middleware.rb
@@ -1,5 +1,5 @@
-# middleware used only in development for testing purposes
 require_relative './helper'
+# middleware used only in development for testing purposes
 class RequestMiddleware
   include Helper
   def request(client, head, body)
